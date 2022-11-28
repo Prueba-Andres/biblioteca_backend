e8d06952092efcb698347c30c9efe6e6a81c48ed
fabruaal <111102040+fabruaal@users.noreply.github.com>
2022-11-27 19:25:04 -0500

    Se arego el @crossOrigin en el controller de libto y tematica, adicionalmente se creo un objeto que permite guardar una biblioteca sin qye el usaario lo digite

--- src/main/java/com/biblioteca/biblioteca_backend/controller/LibroRestController.java
+++ src/main/java/com/biblioteca/biblioteca_backend/controller/LibroRestController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/libro/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class LibroRestController {
 
     @Autowired
--- src/main/java/com/biblioteca/biblioteca_backend/service/LibroServiceImp.java
+++ src/main/java/com/biblioteca/biblioteca_backend/service/LibroServiceImp.java
@@ -1,10 +1,13 @@
 package com.biblioteca.biblioteca_backend.service;
 
+import com.biblioteca.biblioteca_backend.model.Biblioteca;
 import com.biblioteca.biblioteca_backend.model.Libro;
+import com.biblioteca.biblioteca_backend.repository.IBibliotecaRepository;
 import com.biblioteca.biblioteca_backend.repository.ILibroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,9 +16,13 @@ public class LibroServiceImp implements ILibroService {
 
     @Autowired
     private ILibroRepository libroRepository;
-
     @Override
     public Libro crearLibro(Libro libro) {
+        //se creo para enviar la biblioteca
+        Biblioteca biblioteca = new Biblioteca(1, "Departamental");
+        libro.setBiblioteca(biblioteca);
+        //
+
         Libro crearLibro = libroRepository.save(libro);
         return crearLibro;
     }
